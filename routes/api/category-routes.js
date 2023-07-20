1bac4302805374ad7829aaec1ff9bfd32db30344
Joshua Hebert <jhebertcocktails@gmail.com>
2023-07-20 17:38:07 -0400

    category-routes updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
     // find all categories
     const categories = await Category.findAll({
       // be sure to include its associated Products
-      include: [{ model: Product }],
+      include: { model: Product, },
     });
     res.status(200).json(categories);
   } catch (error) {
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
     // find one category by its `id` value
     const category = await Category.findByPk(req.params.id, {
       // be sure to include its associated Products
-      include: [{ model: Product }],
+      include: { model: Product, },
     });
     if (!category) {
       res.status(404).json({ message: 'No category found with that id!' });
@@ -39,7 +39,7 @@ router.post('/', async (req, res) => {
     const newCategory = await Category.create(req.body);
     res.status(200).json(newCategory);
   } catch (error) {
-    res.status(400).json(error);
+    res.status(400).json({message: "newCategory not created"});
   }
 });
 
@@ -48,13 +48,9 @@ router.put('/:id', async (req, res) => {
     // update a category by its `id` value
     const updateCategory = await Category.update(req.body, {
       where: {
-        category_id: req.params.id,
+        id: req.params.id,
       },
     });
-    if (!updateCategory) {
-      res.status(404).json({ message: 'No category found with that id!' });
-      return;
-    }
     res.status(200).json(updateCategory);
   } catch (error) {
     res.status(500).json(error);
@@ -66,13 +62,9 @@ router.delete('/:id', async (req, res) => {
     // delete a category by its `id` value
     const deleteCategory = await Category.destroy({
       where: {
-        category_id: req.params.id,
+        id: req.params.id,
       },
     });
-    if (!deleteCategory) {
-      res.status(404).json({ message: 'No category found with that id!' });
-      return;
-    }
     res.status(200).json(deleteCategory);
   } catch (error) {
     res.status(500).json(error);

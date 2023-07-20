ec3f18e788ec321ba375acb38c4d9bd1a6ec28a1
Joshua Hebert <jhebertcocktails@gmail.com>
2023-07-19 21:57:27 -0400

    defined columns for tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -7,8 +7,20 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      field: 'id',
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: 'tag_name',
+    },
   },
-  {
+    {
     sequelize,
     timestamps: false,
     freezeTableName: true,

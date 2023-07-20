1e07da5df4922fc93d5e37730f9ce0012d6e9036
Joshua Hebert <jhebertcocktails@gmail.com>
2023-07-19 22:38:07 -0400

    updated columns

--- models/Product.js
+++ models/Product.js
@@ -16,31 +16,39 @@ Product.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    product_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     price: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.DECIMAL,
       allowNull: false,
+      validate: {
+        isDecimal: true,
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
     },
   },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        references: {
+          model: 'category',
+          key: 'id',
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+        },
+      },
+    },
+  },
+},
   {
     sequelize,
     timestamps: false,

5213fdfba510086510c5bcca9f4a83ee2fa151c2
Joshua Hebert <jhebertcocktails@gmail.com>
2023-07-20 21:38:29 -0400

    updated index.js deleted as

--- models/index.js
+++ models/index.js
@@ -8,27 +8,23 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
-  as: 'product_category'
 });
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
-  as: 'category_products',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
   onDelete: 'CASCADE',
-  as: 'product_tags',
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(ProductTag, {
   through: ProductTag,
   foreignKey: 'tag_id',
   onDelete: 'CASCADE',
-  as: 'tag_products',
 });
 
 module.exports = {
